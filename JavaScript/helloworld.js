4d3b87ef4b927edb2b95c13126b69d3ef11d8549
ygor_gribeiro <ygorribeiro1997@hotmail.com>
2022-07-19 19:27:52 -0300

    Primeiro commit

---
+++ JavaScript/helloworld.js
@@ -0,0 +1,11 @@
+var texto = "Hello World"
+
+function imprimirTexto(textoParaImprimir){
+console.log(textoParaImprimir);
+}
+
+imprimirTexto(texto);
+
+var linguagens = ["JavaScript", "PHP", "JAVA", "C#"];
+
+console.log(linguagens.length);
